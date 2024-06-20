12da8c887e68a401fcfeba6bd6ea230a88c3991c
Fabio Oliveira <fabio.oliveira.prestador@prf.gov.br>
2024-06-20 12:49:28 -0300

    Relacionamento entre Usuario e Acesso

---
+++ src/main/java/com/fabio/loja_virtual/model/Usuario.java
@@ -0,0 +1,82 @@
+package com.fabio.loja_virtual.model;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "usuario")
+@SequenceGenerator(name = "seq_usuario", sequenceName = "seq_usuario", initialValue = 1, allocationSize = 1)
+public class Usuario implements UserDetails, Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
+    private Long id;
+
+    private String login;
+
+    private String senha;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataAtualSenha;
+
+    /**
+     * Atributo que define a relação de um para muitos entre a entidade Usuario e Acesso, configurada para carregamento preguiçoso (LAZY).
+     * A relação é mapeada através de uma tabela de junção chamada 'usuario_acesso', que possui restrições de unicidade para as colunas 'usuario_id' e 'acesso_id',
+     * garantindo que cada par de usuário e acesso seja único na tabela.
+     * A tabela de junção 'usuario_acesso' é especificada com os seguintes detalhes:
+     * - 'usuario_id': coluna que referencia a chave primária 'id' da tabela 'usuario', atuando como uma chave estrangeira.
+     * - 'acesso_id': coluna que referencia a chave primária 'id' da tabela 'acesso', também atuando como uma chave estrangeira.
+     * As restrições de chave estrangeira são definidas para ambas as colunas, garantindo a integridade referencial entre as tabelas 'usuario' e 'acesso'.
+     */
+    @OneToMany(fetch = FetchType.LAZY) //só carrega se for solicitado
+    @JoinTable(name = "usuario_acesso", uniqueConstraints = @UniqueConstraint(columnNames = {"usuario_id", "acesso_id"},
+            name = "unique_aceesso_user"),
+            joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario",
+                    unique = false, foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)),
+            inverseJoinColumns = @JoinColumn(name = "acesso_id", unique = false, referencedColumnName = "id", table = "acesso",
+                    foreignKey = @ForeignKey(name = "acesso_fk", value = ConstraintMode.CONSTRAINT)))
+    private List<Acesso> acessos;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.acessos;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
