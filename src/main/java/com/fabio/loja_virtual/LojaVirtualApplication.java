671026d09a655c1d93f382f7911f2395de2f3f79
Fabio Oliveira <fabio.oliveira.prestador@prf.gov.br>
2024-07-01 09:31:20 -0300

    Ajustes flyWay

--- src/main/java/com/fabio/loja_virtual/LojaVirtualApplication.java
+++ src/main/java/com/fabio/loja_virtual/LojaVirtualApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @ComponentScan(basePackages = {"com.*"})
 @EnableJpaRepositories(basePackages = {"com.fabio.loja_virtual.repository"})
 @EnableTransactionManagement
+@Profile("dev")
 public class LojaVirtualApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/fabio/loja_virtual/db/migration/V1__init.sql
+++
@@ -1,6 +0,0 @@
-SELECT constraint_name
-FROM information_schema.constraint_column_usage
-WHERE table_name = 'usuario_acesso' and column_name = 'acesso_id'
-and constraint_name<> 'unique_acesso_user';
-
-alter table usuario_acesso  drop constraint "uk_fhwpg5wu1u5p306q8gycxn9ky";
